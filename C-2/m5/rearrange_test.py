e6ae494eca8d24da9f5eb00579750e0d12b418f0
QueenChangge <agungcitra2717@gmail.com>
2024-09-28 20:48:44 +0800

    learn basic unit testing

---
+++ C-2/m5/rearrange_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from rearrange import rearrange_name
+
+class TestRearrange(unittest.TestCase):
+    
+  def test_basic(self):
+    testcase = "Lovelace, Ada"
+    expected = "Ada Lovelace"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+# Run the tests
+unittest.main()
